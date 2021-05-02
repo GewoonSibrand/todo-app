e0fca6adb781e4d34e72c3d73fc76c108aa8289f
Sibrand <sibrand1995@gmail.com>
2021-05-02 11:28:39 +0200

    add tailwind and dark mode logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ThemeToggler/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters } from 'vuex'
+import ThemeToggler from './components/ThemeToggler'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ThemeToggler,
+  },
+  beforeMount() {
+    this.$store.dispatch("initTheme")
+  },
+  computed: {
+    ...mapGetters({ theme: 'getTheme' }),
+  },
+  watch: {
+    theme(newTheme) {
+      newTheme === 'light' 
+      ? document.documentElement.classList.remove('dark')
+      : document.documentElement.classList.add('dark')
+    }
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ThemeToggler.vue
@@ -0,0 +1,17 @@
+<template>
+    <button 
+     @click="toggleTheme"
+     class="">
+        <img class="" src="@/assets/img/icon-moon.svg" alt="dark mode icon">
+    </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleTheme() {
+      this.$store.dispatch("toggleTheme");
+    },
+  },
+};
+</script>
